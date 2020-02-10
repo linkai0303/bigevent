64ad4cd5b603751dd061023ae3da3145e35e5788
linkai <695658177@qq.com>
2020-02-10 15:25:09 +0800

    文章删除功能

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -4,13 +4,13 @@ var article = {
     get: function (page, type, state) {
         return $.get(APILIST.article_get, { 'page': page, 'type': type, 'state': state })
     },
+    // 删除文章类别
+    del: function (id) {
+        return $.get(APILIST.article_del, { 'id': id })
+    },
     // 新增文章类别
     // add: function (name, slug) {
     //     return $.post(APILIST.category_add, { 'name': name, 'slug': slug })
-    // },
-    // // 删除文章类别
-    // del: function (id) {
-    //     return $.post(APILIST.category_del, { 'id': id })
     // },
     // // 编辑文章类别
     // edit: function (id, name, slug) {
--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -21,4 +21,6 @@ var APILIST = {
 
     // 文章获取
     article_get: BASEURL + '/admin/search',
+    // 文章删除
+    article_del: BASEURL + '/admin/article_delete',
 }
